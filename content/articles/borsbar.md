429c730001a2c653a94ccfde5b7ed8be04e6c812
Anders Rye Jacobsen <andersrye.jacobsen@gmail.com>
2019-02-18 18:13:11 +0100

    Updated everything...

---
+++ content/articles/borsbar.md
@@ -0,0 +1,24 @@
+Title: Developing StockMarket Bar Software
+Date: 2019-02-18 19:30
+Modified: 2019-02-18 19:30
+Category: Python
+Tags: pandas, data, algorithms, full-stack development, databases, QT
+Slug: borsbar
+Authors: Anders Rye Jacobsen
+Summary: What I learned as an Economics student, who went solo and developed a full-stack system.
+<hr>
+
+##What have I gotten myself into...
+
+I recently got the job as a board member of a voluntary student organization at my university. 
+The organization is called FinanceLab and it's activities revolves around connecting
+students at the faculty for Economics and Management and top-tier financial companies and organizations.
+
+Due to the very particular interest of our members, FinanceLab has every year held a 
+stock-market bar at the university. A grand event with more than 1200 participants each year!
+The concept of Stock
+
+```python
+s = "Python syntax highlighting"
+print(s)
+```
---
+++ pelicanconf.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+PATH = 'content'
+
+THEME = 'FlexTheme'
+
+AUTHOR = 'Anders Rye Jacobsen'
+SITETITLE = AUTHOR
+SITENAME = 'Anders\' Blog'
+SITESUBTITLE = '<br>Economics student who loves everything<br> technology, coding and data-science'
+SITEDESCRIPTION = 'My thoughts and writings about technology, data-science, life and everything in between'
+
+FAVICON = '/images/favicon.ico'
+BROWSER_COLOR = '#333333'
+PYGMENTS_STYLE = 'default'
+SITEURL = 'http://localhost:8000'
+
+TIMEZONE = 'Europe/Paris'
+
+COPYRIGHT_YEAR = f'{AUTHOR} 2019'
+
+DEFAULT_LANG = 'en'
+OG_LOCALE = 'en_US'
+LOCALE = 'en_US'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+MAIN_MENU = True
+MENUITEMS = (('Archive', '/archives'), ('Categories', '/categories'),)
+
+# Social widget
+SOCIAL = (('linkedin', 'https://linkedin.com/in/anders-rye-jacobsen'),
+          ('github', 'https://github.com/ismand95'),)
+
+DEFAULT_PAGINATION = 7
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
